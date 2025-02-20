752592684e278b2a46bdb328c1af65bf0d0561a9
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-02-20 15:02:30 +0700

    ok

---
+++ Part4/Part4/MovieRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using static MovieSeries.DataAccessLayer.Program;
+
+
+
+namespace MovieSeries.DataAccessLayer
+{
+    public class MovieRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MovieRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all movies
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // Add a new movie
+        public async Task AddMovieAsync(Movie movie)
+        {
+            await _context.Movies.AddAsync(movie);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Part4/Part4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+
+
+
+namespace MovieSeries.DataAccessLayer
+{
+
+
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
